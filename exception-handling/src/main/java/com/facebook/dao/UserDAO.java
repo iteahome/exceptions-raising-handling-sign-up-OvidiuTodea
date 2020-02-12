7b114e40c7e2a1ccdc42477c60cf05f79fa75e9b
ovidiu todea <ovi.todea@yahoo.com>
2020-02-12 12:35:57 +0200

    second commit- sign up working (validate user already exists)

--- exception-handling/src/main/java/com/facebook/dao/UserDAO.java
+++ exception-handling/src/main/java/com/facebook/dao/UserDAO.java
@@ -8,6 +8,7 @@ import com.facebook.model.User;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class UserDAO {
 
@@ -15,15 +16,19 @@ public class UserDAO {
 
     public List<User> readAllUsers() throws FbTechnicalException {
         List<User> userList = new ArrayList<>();
-        try (BufferedReader userReader = new BufferedReader(new FileReader(USERS_FILE))) {
-            String userLine = userReader.readLine();
-            String[] userValues = userLine.split(";");
+        Scanner scannerText=null;
+        try {
+            scannerText=new Scanner(new BufferedReader(new FileReader(USERS_FILE)));
+            String userLine="";
+            while(scannerText.hasNextLine()){
+                userLine = scannerText.nextLine();
+                String[] userValues = userLine.split(";");
 
-            userList.add(new User(userValues[0], userValues[1]));
+                userList.add(new User(userValues[0], userValues[1]));
+            }
         } catch (IOException e) {
             throw new FbFileException("Error reading users", e);
         }
-
         return userList;
     }
 
@@ -31,10 +36,10 @@ public class UserDAO {
 
         try (FileWriter fileWriter = new FileWriter(USERS_FILE, true);
              BufferedWriter bufferedWriter = new BufferedWriter(fileWriter)) {
-                bufferedWriter.newLine();
-                bufferedWriter.write(user.getEmail());
-                bufferedWriter.write(";");
-                bufferedWriter.write(user.getPassword());
+            bufferedWriter.newLine();
+            bufferedWriter.write(user.getEmail());
+            bufferedWriter.write(";");
+            bufferedWriter.write(user.getPassword());
         } catch (IOException e) {
             throw new FbFileException("Error writing user to file", e);
         }
--- exception-handling/src/main/java/com/facebook/service/UserService.java
+++ exception-handling/src/main/java/com/facebook/service/UserService.java
@@ -6,39 +6,32 @@ import com.facebook.exception.FbUserExistsAlready;
 import com.facebook.exception.FbWrongCredentialsException;
 import com.facebook.model.User;
 
-import java.util.List;
-
-
 public class UserService {
 
     private UserDAO userDAO = new UserDAO();
 
     public User login(String inputEmail, String inputPassword) throws FacebookException {
-        for (User user: userDAO.readAllUsers()) {
+        for (User user : userDAO.readAllUsers()) {
             if (inputEmail.equals(user.getEmail()) && inputPassword.equals(user.getPassword())) {
                 return user;
             }
         }
-
         throw new FbWrongCredentialsException();
     }
 
     public void signUp(User addedUser) throws FacebookException {
-        for (User user: userDAO.readAllUsers()) {
+
+        boolean userExists = false;
+        for (User user : userDAO.readAllUsers()) {
             if (addedUser.getEmail().equalsIgnoreCase(user.getEmail())) {
-                //if (1==1) {
-                throw new FbUserExistsAlready();
-            } else
-                userDAO.writeUserToFile(addedUser);
+                userExists = true;
+               throw new FbUserExistsAlready();
+            }
         }
 
-//        List<User> userList = userDAO.readAllUsers();
-//        for (User user: userList) {
-//            if (!(addedUser.getEmail().equals(user.getEmail()))) {
-//                userDAO.writeUserToFile(addedUser);
-//            }
-//        } throw new FbUserExistsAlready();
-
+        if (!userExists) {
+            userDAO.writeUserToFile(addedUser);
+        }
     }
-
 }
+
